cb3d01b6e54897eaeb9027ec736035ff7aa1cbf0
cinhoo <ch05042210@gmail.com>
2016-04-19 16:24:15 +0800

    fixed crash when opened from the notification

--- app/src/main/java/com/haoutil/xposed/haoblocker/fragment/SMSFragment.java
+++ app/src/main/java/com/haoutil/xposed/haoblocker/fragment/SMSFragment.java
@@ -6,6 +6,7 @@ import android.os.Environment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,6 +42,8 @@ public class SMSFragment extends BaseFragment implements BaseRecycleAdapter.OnIt
         activity = (SettingsActivity) getActivity();
         blockerManager = new BlockerManager(activity);
         blockerManager.readAllSMS();
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -55,6 +58,14 @@ public class SMSFragment extends BaseFragment implements BaseRecycleAdapter.OnIt
             rv_sms.setAdapter(adapter);
         }
         return view;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        menu.findItem(R.id.export).setVisible(true);
+        menu.findItem(R.id.import0).setVisible(true);
+        activity.setOnMenuItemClickListener(this);
     }
 
     @Override
@@ -170,17 +181,6 @@ public class SMSFragment extends BaseFragment implements BaseRecycleAdapter.OnIt
                 }
             }
         }.run();
-    }
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if (activity != null) {
-            activity.setOnMenuItemClickListener(
-                    isVisibleToUser ? this : null,
-                    isVisibleToUser ? SettingsActivity.SHOW_EXPORT | SettingsActivity.SHOW_IMPORT : SettingsActivity.SHOW_NONE
-            );
-        }
     }
 
     @Override
